12239fb4d8caae81cba5dd40cb6ab102932d44d0
Glarmo <guillermo@ewertcs08.doverbroecks.com>
2014-10-16 15:52:18 +0100

    stuff

--- project police/Assets/scripts/randomInstance.cs
+++ project police/Assets/scripts/randomInstance.cs
@@ -4,23 +4,41 @@ using System.Collections;
 public class randomInstance : MonoBehaviour
 {
     public GameObject marker;
-    public Vector2 spawnValues;
-    private int random;
+    public Vector2 mapLimits;
+    private int chance;
     public static int count;
+    public Texture2D roads;
+    
 
-	void Update () 
+	/*void Update ()
     {
-
-                random = Random.Range(0, 1000);
-                if (random < 10 && count < 10)
+                int hori = (int) (Random.Range (0, mapLimits.x));
+                int vert = (int) (Random.Range (0, mapLimits.y));
+                Color testColor = roads.GetPixel (hori, vert);
+                print (testColor);
+                chance = Random.Range(0, 1000);
+                if (chance < 10 && count < 10)
                 {
-                    Vector2 spawnPosition = new Vector2 (Random.Range (-spawnValues.x, spawnValues.x), Random.Range (-spawnValues.y, spawnValues.y));
+                    Vector2 spawnPosition = new Vector2 (hori/200.6f, -vert/200.6f);
                     Quaternion spawnRotation = Quaternion.identity;
                     Instantiate (marker, spawnPosition, spawnRotation);
                     count ++;
                 }
 
 
-	}
+	}*/
+    
+    void Start ()
+    {
+                int hori = (int) (Random.Range (0, mapLimits.x));
+                int vert = (int) (Random.Range (0, mapLimits.y));
+                Color testColor = roads.GetPixel (hori, vert);
+                print (testColor);
+                print (hori);
+                print (vert);
+                Vector2 spawnPosition = new Vector2 (hori/200.6f, -vert/200.6f);
+                Quaternion spawnRotation = Quaternion.identity;
+                Instantiate (marker, spawnPosition, spawnRotation);
+    }
     
 }
