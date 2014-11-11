00625d591041e698b7d26f49df6e8b66d1032473
Glarmo <guillermo@ewertcs08.doverbroecks.com>
2014-11-11 11:19:22 +0000

    start of navmesh

---
+++ project police/Assets/scripts/movement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movement : MonoBehaviour 
+{
+	public Transform target;
+	private NavMeshAgent navComp;
+	
+	void Start () 
+	{
+		navComp = transform.GetComponent (NavMeshAgent);
+	}
+
+	void Update () 
+	{
+		if (target) 
+		{
+			navComp.SetDestination(target.position);
+		}
+	}
+}
