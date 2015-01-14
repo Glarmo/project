01a2c245ce30eabc9acb2db873f416339c99bad4
Glarmo <guillermo@ewertcs08.doverbroecks.com>
2015-01-14 14:33:34 +0000

    smoother camera
    negative mouse postion on top not working still

--- project police/Assets/_Scripts/cameraDragV4.cs
+++ project police/Assets/_Scripts/cameraDragV4.cs
@@ -12,35 +12,80 @@ public class cameraDragV4 : MonoBehaviour
 	public int maxZ;
 	public int scrollDistance = 5; 
 	public int scrollSpeed = 70;
+	public int scrollScalingSpeed; 
 
-
-
+	private float xLeftScaling;
+	private float xRightScaling;
+	private float yBottomScaling;
+	private float yTopScaling;
 	
 	// Update is called once per frame
 	void Update () 
 	{
 		zoomCamera ();
+
 		float mousePosX = Input.mousePosition.x; 
 		float mousePosY = Input.mousePosition.y; 
+
+		xLeftScaling = -scrollSpeed / mousePosX * scrollScalingSpeed;
+		xRightScaling = scrollSpeed / (Screen.width - mousePosX) * scrollScalingSpeed;
+		yBottomScaling = scrollSpeed / mousePosY * scrollScalingSpeed;
+		yTopScaling = -scrollSpeed / (Screen.height - mousePosY) * scrollScalingSpeed;
+
+		if (xLeftScaling > 70) 
+		{
+			xLeftScaling = 70;
+		}
+
+		if (xRightScaling > 70)
+		{
+			xRightScaling = 70;
+		}
+
+		if (yBottomScaling > 70)
+		{
+			yBottomScaling = 70;
+		}
+
+		if (yTopScaling > 70)
+		{
+			yTopScaling = 70;
+		}
 		
 		if (mousePosX < scrollDistance) 
 		{ 
-			transform.Translate(Vector3.right * -scrollSpeed * Time.deltaTime); 
+			if (xLeftScaling < 0)
+			{
+				xLeftScaling = -70;
+			}
+			transform.Translate(Vector3.right * xLeftScaling * Time.deltaTime); 
 		} 
 		
 		if (mousePosX >= Screen.width - scrollDistance) 
 		{ 
-			transform.Translate(Vector3.right * scrollSpeed * Time.deltaTime); 
+			if (xRightScaling < 0)
+			{
+				xRightScaling = 70;
+			}
+			transform.Translate(Vector3.right * xRightScaling * Time.deltaTime); 
 		}
 		
 		if (mousePosY < scrollDistance) 
 		{ 
-			transform.Translate(transform.forward * scrollSpeed * Time.deltaTime); 
+			if (yBottomScaling < 0)
+			{
+				yBottomScaling = 70;
+			}
+			transform.Translate(transform.forward * yBottomScaling * Time.deltaTime); 
 		} 
 		
 		if (mousePosY >= Screen.height - scrollDistance) 
 		{ 
-			transform.Translate(transform.forward * -scrollSpeed * Time.deltaTime); 
+			if (yTopScaling < 0)
+			{
+				yTopScaling = -70;
+			}
+			transform.Translate(transform.forward * yTopScaling * Time.deltaTime); 
 		}
 
 		transform.position = new Vector3(Mathf.Clamp(transform.position.x, minX, maxX), 15, Mathf.Clamp(transform.position.z, minZ, maxZ));
